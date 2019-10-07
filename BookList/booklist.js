41cee3f8d30ef0392f82a277abf66619452999e9
musavvirn <musavvir.noor@gmail.com>
2019-10-06 20:27:27 -0400

    Add files via upload

--- BookList/booklist.js
+++ BookList/booklist.js
@@ -27,13 +27,6 @@ class UI {
     ];
 
     const books = StoredBooks;
-
-    // function addListToUI() {
-    //   for (var i = 0; i < StoredBooks.length; i++) {
-    //     UI.addBooksToList(StoredBooks[i])
-    //   }
-    // }
-
     books.forEach((book) => UI.addBookToList(book));
   }
 
@@ -41,24 +34,41 @@ class UI {
     const list = document.getElementById("book-list");
     const row = document.createElement("tr");
 
-    row.innerHTML = `<td> ${book.title} </td><td> ${book.author} </td><td> ${book.isbn} </td><td><a href="#" class="btn btn-danger btn-sm">X</a></td>` ;
+    row.innerHTML = `<td> ${book.title} </td><td> ${book.author} </td><td> ${book.isbn} </td><td><a href="#" class="btn btn-danger btn-sm btn-delete">X</a></td>` ;
     list.appendChild(row);
   }
 
+  static deleteBook(targetRow) {
+    targetRow.parentElement.parentElement.remove();
+  }
+
+  // Clears fields after adding book, resets outlines
   static clearFields() {
+    document.getElementById("book-name").value = "";
+    document.getElementById("book-author").value = "";
+    document.getElementById("book-isbn").value = "";
 
-    document.getElementById("book-name").innerHTML = "asdasds";
-    document.getElementById("book-author").innerHTML = "";
-    document.getElementById("book-isbn").innerHTML = "";
-
-      // document.getElementById("book-name").placeholder = "ABC";
-      // document.getElementById("book-author").placeholder = "Fiction";
-      // document.getElementById("book-isbn").placeholder = "00000";
+    document.getElementById("book-name").style.outline="1px solid black";
+    document.getElementById("book-author").style.outline="1px solid black";
+    document.getElementById("book-isbn").style.outline="1px solid black";
     }
-
 }
 
 // Store Class: store booklist
+
+class Store {
+  static getBooks() {
+
+  }
+
+  static addBook() {
+
+  }
+
+  static removeBook() {
+    
+  }
+}
 
 // Event: display booklist
 
@@ -66,24 +76,70 @@ document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: add a book
 
-// document.getElementById("submit-button").addEventListener("click", addBook());
-
 window.onload = function() {
-document.getElementById("submit-button").addEventListener("click", function(){
+  var btnSubmit = document.getElementById("submit-button");
+  btnSubmit.onclick = submitButton;
 
+  addDeleteAction();
+  }
+
+// Adds delete row functionality to all delete buttons
+function addDeleteAction() {
+    var btnDelete = document.getElementsByClassName("btn-delete");
+    for (var i = 0; i < btnDelete.length; i++) {
+      btnDelete[i].onclick = deleteRow;
+    }
+}
+
+function deleteRow() {
+  UI.deleteBook(event.target);
+}
+
+// Checks each fied for validations, alerts if not valid, finally adds book
+function submitButton() {
+  const fieldList = document.getElementsByClassName("book-fields");
+  var incompleteField = false;
+  var completefields = true;
+
+  for (var i = 0; i < fieldList.length; i++) {
+    validateFields(fieldList[i]);
+
+    if (fieldList[i].value=="") {
+      if (!incompleteField) {
+        alert("Check fields!")
+        incompleteField = true;
+        completefields = false;
+      }
+    }
+  }
+
+  if (completefields) {
+    addBook();
+  }
+}
+
+// Validates a field, consumes field parameter
+function validateFields(field) {
+  if (field.value=="") {
+    field.style.outline="1px solid red";
+  } else {
+    field.style.outline="1px solid black";
+  }
+}
+
+// Create new book, calls UI to add book to display, calls clear fields
+function addBook() {
   const title = document.getElementById("book-name").value;
   const author = document.getElementById("book-author").value;
   const isbn = document.getElementById("book-isbn").value;
+  const newBook = new book(title, author, isbn);
 
-  const book1 = new book(title, author, isbn);
-
-  UI.addBookToList(book1);
-
+  UI.addBookToList(newBook);
   UI.clearFields();
-
-
-
-})
 }
+
+// Deletes a row of book
+
+
 
 // Event: delete a book
