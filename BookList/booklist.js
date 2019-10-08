a2fa861b3cb5fe4d31f49c412f9c8f37ff24d997
musavvirn <musavvir.noor@gmail.com>
2019-10-07 21:39:45 -0400

    Add files via upload

--- BookList/booklist.js
+++ BookList/booklist.js
@@ -1,10 +1,11 @@
 //Book Class: Make new books
 
 class book {
-  constructor(title, author, isbn) {
+  constructor(title, author, isbn, dateAdded) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
+    this.dateAdded = dateAdded;
 
   }
 }
@@ -17,12 +18,14 @@ class UI {
       {
         title: "Harry Potter",
         author: "Fiction",
-        isbn: "1000"
+        isbn: "1000",
+        dateAdded: "01/01/1900, 2:35:56 AM"
       },
       {
         title: "Robin Hood",
         author: "Historical",
-        isbn: "2000"
+        isbn: "2000",
+        dateAdded: "01/01/1900, 2:35:56 AM"
       }
     ];
 
@@ -34,12 +37,33 @@ class UI {
     const list = document.getElementById("book-list");
     const row = document.createElement("tr");
 
-    row.innerHTML = `<td> ${book.title} </td><td> ${book.author} </td><td> ${book.isbn} </td><td><a href="#" class="btn btn-danger btn-sm btn-delete">X</a></td>` ;
+    row.innerHTML = `<td id="x1"> ${book.title} </td><td id="x2"> ${book.author} </td><td id="x3"> ${book.isbn} </td><td id="x4"> ${book.dateAdded} </td><td id="x5"><a href="#" class="btn btn-danger btn-sm btn-delete">X</a></td>` ;
     list.appendChild(row);
   }
 
   static deleteBook(targetRow) {
     targetRow.parentElement.parentElement.remove();
+  }
+
+  static displaySearchBooks(searchText) {
+    const rowList = document.getElementById("book-list").children;
+
+    for (var i = 0; i < rowList.length; i++) {
+      const rowListChildren = rowList[i].children;
+      var matched = false;
+
+      for (var j = 0; j < 3; j++) {
+
+        var col = rowListChildren[j];
+        if (!matched && col.innerHTML.toUpperCase().search(searchText.toUpperCase()) >= 0) {
+          console.log(col.innerHTML.toUpperCase());
+          col.parentElement.style.display = "";
+          break;
+        } else {
+          col.parentElement.style.display = "none";
+        }
+      }
+    }
   }
 
   // Clears fields after adding book, resets outlines
@@ -52,21 +76,18 @@ class UI {
     document.getElementById("book-author").style.outline="1px solid black";
     document.getElementById("book-isbn").style.outline="1px solid black";
     }
-}
 
-// Store Class: store booklist
+  static hideColumn(column) {
+    var columnId = column.parentElement.id;
+    var rowList = document.getElementById("book-list").children;
+    document.getElementById("x").innerHTML = rowList.length;
 
-class Store {
-  static getBooks() {
+    for (var i = 0; i < rowList.length; i++) {
+      rowList[i].children[columnId.charAt(1)-1].innerHTML = "";
+      // rowList[i].children[columnId-1].style.color = "red";
+    }
 
-  }
-
-  static addBook() {
-
-  }
-
-  static removeBook() {
-    
+    column.innerHTML = "&#x2B07;";
   }
 }
 
@@ -80,8 +101,55 @@ window.onload = function() {
   var btnSubmit = document.getElementById("submit-button");
   btnSubmit.onclick = submitButton;
 
-  addDeleteAction();
+  var tableRow = document.getElementsByTagName("tr");
+  for (var i = 0; i < tableRow.length; i++) {
+    tableRow[i].onmouseover = hoverName;
+    tableRow[i].onmouseout = hoverNameDisable;
   }
+
+  var filterInput = document.getElementById("book-search");
+  filterInput.addEventListener("keyup", filterNames);
+
+  addDeleteAction();
+  addColumHideAction();
+
+  // var xbtn = document.getElementById("x");
+  // x.onclick = test();
+}
+
+function filterNames() {
+  // console.log("1");
+  var searchText = document.getElementById("book-search").value;
+  if (searchText.replace("/\s/g", "").length) {
+    UI.displaySearchBooks(searchText);
+  } else {
+    UI.displaySearchBooks("");
+  }
+}
+
+function test() {
+  var d = new Date();
+  var n = d.toLocaleString();
+  alert(n + "sdsad");
+}
+
+function hoverName() {
+  var descBox = document.getElementById("card-desc");
+  var x = event.clientX;
+  var y = event.clientY;
+
+  descBox.style.position = "absolute";
+  descBox.style.top = y + 'px';
+  descBox.style.left = x + 'px';
+
+  descBox.innerHTML = event.target.innerHTML;
+}
+
+function hoverNameDisable() {
+  var descBox = document.getElementById("card-desc");
+  descBox.style.display = "none";
+}
+
 
 // Adds delete row functionality to all delete buttons
 function addDeleteAction() {
@@ -89,6 +157,18 @@ function addDeleteAction() {
     for (var i = 0; i < btnDelete.length; i++) {
       btnDelete[i].onclick = deleteRow;
     }
+}
+
+// Adds hide column functionality
+function addColumHideAction() {
+    var columnHide = document.getElementsByClassName("column-hide");
+    for (var i = 0; i < columnHide.length; i++) {
+      columnHide[i].onclick = toggleColumn;
+    }
+}
+
+function toggleColumn() {
+  UI.hideColumn(event.target);
 }
 
 function deleteRow() {
@@ -132,7 +212,8 @@ function addBook() {
   const title = document.getElementById("book-name").value;
   const author = document.getElementById("book-author").value;
   const isbn = document.getElementById("book-isbn").value;
-  const newBook = new book(title, author, isbn);
+  const dateAdded = new Date().toLocaleString();
+  const newBook = new book(title, author, isbn, dateAdded);
 
   UI.addBookToList(newBook);
   UI.clearFields();
